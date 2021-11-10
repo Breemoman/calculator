3b87ff7877b6c423bd287722ccc808fedef992cd
Breemoman <93917736+Breemoman@users.noreply.github.com>
2021-11-10 23:02:42 +0300

    Add files via upload

---
+++ optimised_calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int first_number;
+    int second_number;
+    string op;
+    cout << "enter first number: " << endl;
+    cin >> first_number;
+    cout << "enter second number: " << endl;
+    cin >> second_number;
+    cout << "enter operator: " << endl;
+    cin >> op;
+    if (op == "+") {cout << first_number + second_number;}
+    else if (op == "-") {cout << first_number - second_number;}
+    else if (op == "*") {cout << first_number * second_number;}
+    else if (op == "/") {cout << first_number / second_number;}
+    else cout << "invalid operator" << endl;
+    return 0;
+}
