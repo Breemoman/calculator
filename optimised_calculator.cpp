78f55f07549040495fdace274a1c103a1723ebec
Kaank2006 <kaankucuk2006@gmail.com>
2021-12-03 21:08:24 +0300

    Optimised further

--- optimised_calculator.cpp
+++ optimised_calculator.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main() {
+
     int first_number, second_number;
-    
-    string op;
+    char op;
     cout << "enter first number: " << endl;
     cin >> first_number;
+
     cout << "enter second number: " << endl;
     cin >> second_number;
+
     cout << "enter operator: " << endl;
     cin >> op;
-    if (op == "+") {cout << first_number + second_number;}
-    else if (op == "-") {cout << first_number - second_number;}
-    else if (op == "*") {cout << first_number * second_number;}
-    else if (op == "/") {cout << first_number / second_number;}
-    else cout << "invalid operator" << endl;
-    return 0;
+
+    switch(op){
+        case '+':
+            cout << first_number + second_number << endl;
+            break;
+
+        case '-':
+            cout << first_number - second_number << endl;
+            break;
+
+        case '*':
+            cout << first_number * second_number << endl;
+            break;
+
+        case '/':
+            cout << first_number / second_number << endl;
+            break;
+
+        default:
+            cout << "invalid operator" << endl;
+            break;
+    }
+    return EXIT_SUCCESS;
 }
